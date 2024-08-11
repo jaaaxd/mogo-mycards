2c8882fdabaa09d3a6712ff5671340e55330908b
Ja <ja.techup@gmail.com>
2024-08-11 14:10:51 +0700

    fix: re-install packages to fix deployment errors

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,5 @@
 import "@/styles/globals.css";
-import { Quicksand } from '@next/font/google';
-
-const quicksand = Quicksand({
-  weight: ['400', '500', '600', '700'], // Specify the font weights you need
-  subsets: ['latin'], // Specify the subsets you need
-});
 
 export default function App({ Component, pageProps }) {
-  return (
-    <div className={quicksand.className}>
-      <Component {...pageProps} />
-    </div>
-  );
+  return <Component {...pageProps} />;
 }
