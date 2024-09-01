d17f5ee02d3e9c34914b5fe3f36e775dbd12ab4d
Ja <ja.techup@gmail.com>
2024-09-02 00:55:28 +0700

    fix: adjust card img object fit

--- components/CardAlbums.js
+++ components/CardAlbums.js
@@ -19,20 +19,20 @@ export default function CardAlbums() {
             {cards.map((card, index) => (
               <div
                 key={index}
-                className={`card-container flex flex-col w-full max-w-[150px] items-center justify-end space-x-2 sm:p-3 px-[7px] py-[2px]  $`}
+                className={`card-container flex flex-col w-full max-w-[150px] items-center justify-end space-x-2 sm:mx-1 sm:my-1 px-[7px] py-[2px]  $`}
                 onClick={() => handleCardClick(index)}
               >
                 <div className="card-image relative">
                   <img
                     src={card.img}
                     alt={card.name}
-                    className={`w-full h-full object-cover mb-2 ${
+                    className={`w-full h-full object-contain mb-2 ${
                       card.status === 3 ? "grayscale" : ""
                     }`}
                   />
 
                   {card.status === 2 && (
-                    <div className="absolute bottom-[33px] left-0 rounded-[2px] lg:text-[19px] text-gray-300 bg-white pl-2 pr-3 max-h-[32px] lg:h-7 border w-[45px] lg:w-[55px] text-center border-gray-100 justify-center items-center flex">
+                    <div className="absolute bottom-[37px] left-0 rounded-[2px] lg:text-[19px] text-gray-300 bg-white pl-2 pr-3 max-h-[32px] lg:h-7 border w-[45px] lg:w-[55px] text-center border-gray-100 justify-center items-center flex">
                       +1
                     </div>
                   )}
