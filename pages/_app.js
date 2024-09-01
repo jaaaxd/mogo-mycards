4dc1d2f82e6f653857b585452c6af7accd54960b
Ja <ja.techup@gmail.com>
2024-09-01 22:29:45 +0700

    feat: page title and Google Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,16 @@
 import "@/styles/globals.css";
+import Head from "next/head";
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>MOGO Mycards - Trading Message Generator</title>
+        <link rel="icon" href="/logo.png" />
+      </Head>
+      <GoogleAnalytics gaId={process.env.GA_ID} />
+      <Component {...pageProps} />
+    </>
+  );
 }
